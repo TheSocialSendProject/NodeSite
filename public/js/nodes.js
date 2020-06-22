93d4a84917931f534bf23492353314dfd601c302
Jon <squid@sqdmc.net>
2020-06-22 08:28:05 -0400

    Update for socialsend.io
    Signed-off-by: Jon <squid@sqdmc.net>

--- public/js/nodes.js
+++ public/js/nodes.js
@@ -1,32 +1,85 @@
 
-ajax_load_block();
-ajax_load_conns();
+setTimeout(ajax_load_block(), 100);
+setInterval(ajax_load_block, 38800);
+setTimeout(ajax_load_conns(), 500);
+setInterval(ajax_load_conns, 64200);
 
 function ajax_load_block() {
   element = "";
   path = "/api/getbestblock";
+  document.getElementById("blockhash").classList.add("coloryellow");
+  document.getElementById("blockcount").classList.add("coloryellow");
+  document.getElementById("blockcount").innerHTML = "0000000";
   var xhr = new XMLHttpRequest();
   xhr.open('GET', path, true);
-  xhr.onreadystatechange = function() {
-      if (this.readyState !== 4) return;
-      if (this.status !== 200) return;
+  xhr.ontimeout = function () {
+    document.getElementById("blockhash").classList.remove("coloryellow");
+    document.getElementById("blockhash").classList.add("colorred");
+    document.getElementById("blockcount").classList.remove("coloryellow");
+    document.getElementById("blockcount").classList.add("colorred");
+    return console.error("Request Get Best Block - Request timed out.");
+  };
+  xhr.addEventListener("error", function (event) {
+    document.getElementById("blockhash").classList.remove("coloryellow");
+    document.getElementById("blockhash").classList.add("colorred");
+    document.getElementById("blockcount").classList.remove("coloryellow");
+    document.getElementById("blockcount").classList.add("colorred");
+    console.error(event);
+  });
+  xhr.addEventListener("load", function (event) {
+    if (xhr.status != 200) {
+      document.getElementById("blockhash").classList.remove("coloryellow");
+      document.getElementById("blockhash").classList.add("colorred");
+      document.getElementById("blockcount").classList.remove("coloryellow");
+      document.getElementById("blockcount").classList.add("colorred");
+      document.getElementById("blockcount").innerHTML = "ERROR!!";
+    } else {
       json = JSON.parse(this.responseText);
       document.getElementById("blockhash").innerHTML = json.blockhash;
       document.getElementById("blockcount").innerHTML = json.height;
-  };
+      setTimeout(function() {
+        document.getElementById("blockhash").classList.remove("coloryellow");
+        document.getElementById("blockhash").classList.remove("colorred");
+        document.getElementById("blockcount").classList.remove("coloryellow");
+        document.getElementById("blockcount").classList.remove("colorred");
+      }, 400);
+    }
+  });
+  xhr.timeout = 9000;
   xhr.send();
 }
 
 function ajax_load_conns() {
   element = "";
   path = "/api/getconnectioncount";
+  document.getElementById("networkconn").classList.add("coloryellow");
+  document.getElementById("networkconn").innerHTML = "00";
   var xhr = new XMLHttpRequest();
   xhr.open('GET', path, true);
-  xhr.onreadystatechange = function() {
-      if (this.readyState !== 4) return;
-      if (this.status !== 200) return;
+  xhr.ontimeout = function () {
+    document.getElementById("networkconn").classList.remove("coloryellow");
+    document.getElementById("networkconn").classList.add("colorred");
+    return console.error("Request Get Connections - Request timed out.");
+  };
+  xhr.addEventListener("error", function (event) {
+    document.getElementById("networkconn").classList.remove("coloryellow");
+    document.getElementById("networkconn").classList.add("colorred");
+    console.error(event);
+  });
+  xhr.addEventListener("load", function (event) {
+    if (xhr.status != 200) {
+      document.getElementById("networkconn").classList.remove("coloryellow");
+      document.getElementById("networkconn").classList.add("colorred");
+      document.getElementById("networkconn").innerHTML = "NA";
+    } else {
       json = JSON.parse(this.responseText);
       document.getElementById("networkconn").innerHTML = json.connections;
-  };
+      setTimeout(function() {
+        document.getElementById("networkconn").classList.remove("coloryellow");
+        document.getElementById("networkconn").classList.remove("colorred");
+      }, 400);
+    }
+  });
+  xhr.timeout = 9000;
   xhr.send();
 }
